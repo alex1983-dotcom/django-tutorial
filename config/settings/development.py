5615a8e507d90325f454fd1fde0d4b3f17a57628
mechanic <alebarda2018@gmail.com>
2025-12-11 21:45:46 +0300

    Оьновление конфигов, удаление docker-compose.yml

--- config/settings/development.py
+++ config/settings/development.py
@@ -33,8 +33,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # ← React dev-сервер
 ]
 
-MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
-
 INTERNAL_IPS = ["127.0.0.1", "localhost"]
 DEBUG_TOOLBAR_CONFIG = {"SHOW_TOOLBAR_CALLBACK": lambda request: True}
 
